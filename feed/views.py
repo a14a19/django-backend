12f3f58ded737b9f771ed92cc60cb62ffa57809c
a14a19 <anas31197@gmail.com>
2023-02-21 22:36:34 +0530

    django-backend

--- feed/views.py
+++ feed/views.py
@@ -1,9 +1,12 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post
 
 # Create your views here.
+
+
 class HomePage(ListView):
     http_method_names = ["get"]
     template_name = "feed/homepage.html"
@@ -11,13 +14,26 @@ class HomePage(ListView):
     context_object_name = "posts"
     queryset = Post.objects.all().order_by('-id')[0:30]
 
+
 class PostDetailView(DetailView):
     http_method_name = ["get"]
     template_name = "feed/detail.html"
     model = Post
     context_object_name = "post"
 
-class CreateNewPost(CreateView):
+
+class CreateNewPost(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "feed/create.html"
     fields = ['text']
+    success_url = "/"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.save()
+        return super().form_valid(form)
